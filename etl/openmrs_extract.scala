ede47e595f4e61200226156190f7990746462b92
Michael Italia <italiam@email.chop.edu>
2012-10-01 10:01:15 -0400

    initial ETL script commit

---
+++ etl/openmrs_extract.scala
@@ -0,0 +1,17 @@
+/*
+    This script uses the DataExpress ETL scripting environment to transform data from the 
+    OpenMRS demo data set into the schema for the Harvest demo project.
+
+    Version 1.0
+
+
+    This script requires the DataExpress.jar to be on your java classpath
+
+    //TODO: more details here
+*/
+
+import edu.chop.cbmi.dataExpress.dsl.ETL._
+import edu.chop.cbmi.dataExpress.dsl.stores.SqlDb
+import edu.chop.cbmi.dataExpress.dataModels.RichOption._
+
+
