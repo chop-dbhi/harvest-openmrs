7c1521c6a8a1fb9eb1282bc63e415d4dd122a3c6
Michael Italia <italiam@email.chop.edu>
2012-10-26 15:46:03 -0400

    Update etl to properly remove tables that exist in sqlite first

--- etl/harvest_to_sqlite.scala
+++ etl/harvest_to_sqlite.scala
@@ -37,16 +37,21 @@ val tableQuery = """SELECT relname
 
 val tablenames = DataTable(postgres, tableQuery).map{r => r.relname.as[String].get}.toList
 
-
-tablenames.foreach{ table => sqlite dropTable table}
+//Get rid of any tables that might already be in the sqlite database
+val existingTableQuery = """SELECT name
+                              FROM sqlite_master
+                             WHERE type = 'table'"""
+val existingTables = DataTable(sqlite, existingTableQuery).map{r => r.name.as[String].get}.toList
+existingTables.foreach{ table => println("Dropping sqlite table: %s".format(table))
+                                 sqlite dropTable table}
 
 tablenames.foreach {table =>
-
+    println("Fetching table: %s".format(table))
     val query = """SELECT * from %s""".format(table)
     val sourceTable = DataTable(postgres, query)
 
     val writer = SqlTableWriter(sqlite)
-
+    println("Inserting records into sqlite table")
     writer.insert_table(table, sourceTable.dataTypes, sourceTable, SqlTableWriter.OVERWRITE_OPTION_DROP)
 }
 
