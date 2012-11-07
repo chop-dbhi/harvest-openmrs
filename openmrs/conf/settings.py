32b9b07c9ef2a568c8dccc57131fba37950fb655
Byron Ruth <b@devel.io>
2012-11-07 13:33:58 -0500

    Remove ADMIN_MEDIA_PREFIX Fix warning message for local_settings

--- openmrs/conf/settings.py
+++ openmrs/conf/settings.py
@@ -5,15 +5,13 @@ try:
     from local_settings import *
 except ImportError:
     import warnings
-    warnings.warn('Local settings have not been found (src.conf.local_settings)')
+    warnings.warn('Local settings have not been found (openmrs.conf.local_settings)')
 
 # FORCE_SCRIPT_NAME overrides the interpreted 'SCRIPT_NAME' provided by the
 # web server. since the URLs below are used for various purposes outside of
 # the WSGI application (static and media files), these need to be updated to
 # reflect this alteration
 if FORCE_SCRIPT_NAME:
-    ADMIN_MEDIA_PREFIX = os.path.join(FORCE_SCRIPT_NAME, ADMIN_MEDIA_PREFIX[1:])
-
     STATIC_URL = os.path.join(FORCE_SCRIPT_NAME, STATIC_URL[1:])
     MEDIA_URL = os.path.join(FORCE_SCRIPT_NAME, MEDIA_URL[1:])
 
