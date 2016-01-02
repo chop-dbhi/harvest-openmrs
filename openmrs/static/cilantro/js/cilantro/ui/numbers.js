c4b9fc8a27e6b75d43936d52d2f97e6898c740dc
Byron Ruth <b@devel.io>
2016-01-02 08:50:59 -0500

    Update Harvest components to 2.4
    Signed-off-by: Byron Ruth <b@devel.io>

--- openmrs/static/cilantro/js/cilantro/ui/numbers.js
+++ openmrs/static/cilantro/js/cilantro/ui/numbers.js
@@ -1,2 +1,2 @@
-define(["./core"],function(t){var e=function(e,r,n){n||(n="<em>n/a</em>"),r?e.html(t.utils.prettyNumber(r,t.config.get("threshold"))).attr("title",t.utils.toDelimitedNumber(r)):e.html(n)};return{renderCount:e}});
+define(["./core"],function(t){var e=function(e,n,r){r||(r="<em>n/a</em>"),null===n||void 0===n?e.html(r):e.html(t.utils.prettyNumber(n,t.config.get("threshold"))).attr("title",t.utils.toDelimitedNumber(n))};return{renderCount:e}});
 //# sourceMappingURL=numbers.js.map
